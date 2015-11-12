346a78934f29c3ef9b548206e7026b3336c6934b
Lucas <https://github.com/LucasLemaf/SitePessoal.git>
2015-11-12 08:43:45 -0200

    Foram modificadas as mensagens de erro/sucesso após a validação dos campos do formulario. Agora elas aparecem de acordo com a necessidade do formulário. Retirei do html 1 div e deixei apenas um ID para capturar e fazer a mensagem por JS

--- js/validacao.js
+++ js/validacao.js
@@ -7,42 +7,68 @@ function validar()
 	var nome = form.nome.value;
 	var email = form.email.value;
 	var mensagem = form.mensagem.value;
+	var msg = document.getElementById("a");
+	 
+	msg.style.display = 'block';
 
-	 if (!reg_nome.test(nome)) { 
-	
-		exibir_msg('mensagem_erro');
+	if (!reg_nome.test(nome)) {
+
+		
+		msg.children[0].innerHTML = "Favor preencher o campo 'NOME' corretamente";
+		msg.className = "mensagem_erro";	
 		return;
 	}
 
 	if (!reg_email.test(email)) {
 
-		exibir_msg('mensagem_erro');
+		
+		msg.children[0].innerHTML = "Favor preencher o campo 'EMAIL' corretamente";
+		msg.className = "mensagem_erro";
 		return;
 	}
 	
 	if (mensagem.length > limitemaximo) {
+
 		
-		exibir_msg('mensagem_erro');		
+		msg.children[0].innerHTML = "O campo 'Mensagem' tem limite de 250 caracteres";
+		msg.className = "mensagem_erro";
 		return;
 	}
 
-	exibir_msg('mensagem_sucesso');
+	if (mensagem.length == "") {
+
+		
+		msg.children[0].innerHTML = "O campo Mensagem está vazio";
+		msg.className = "mensagem_erro";
+		return;
+	}
+		
+		msg.className = "mensagem_sucesso";
+		msg.children[0].innerHTML = "Mensagem enviada com Sucesso";
 }
 
-function exibir_msg(class_name) {
+//function exibir_msg(class_name) {
 
-	document.getElementsByClassName(class_name)[0].style.display = 'block';
+//	document.getElementsByClassName("").innerHTML = "Favor preencher os campos";
+//	document.getElementsByClassName("").innerHTML = "Favor preencher os campos corretamente";
+//	document.getElementsByClassName("").innerHTML = "Favor preencher o campo NOME";
+//	document.getElementsByClassName("").innerHTML = "Favor preencher o campo EMAIL";
+//	document.getElementsByClassName("").innerHTML = "Favor preencher o campo Mensagem";
+//	document.getElementsByClassName("").innerHTML = "Favor preencher o campo NOME corretamente";
+//	document.getElementsByClassName("").innerHTML = "Favor preencher o campo EMAIL corretamente";
 
-	setTimeout(sumir_msg, 10000);
-}
+//	setTimeout(sumir_msg, 100000);
+//}
 
 function sumir_msg() 
 {
-	if (document.getElementsByClassName('mensagem_erro')[0].style.display = 'block') {
-		document.getElementsByClassName('mensagem_erro')[0].style.display = 'none'
-	};
-	if (document.getElementsByClassName('mensagem_sucesso')[0].style.display = 'block') {
-		document.getElementsByClassName('mensagem_sucesso')[0].style.display = 'none';
-	}
+	
+	if (document.getElementsByClassName("")[0].style.display = 'block') {
+		document.getElementsByClassName("")[0].style.display = 'none'
+ 	};
+
+ 	if (document.getElementsByClassName("")[0].style.display = 'block') {
+ 		document.getElementsByClassName("")[0].style.display = 'none';
+ 	}
 }
 
